ad0bd779e7df943ce9299ea32f351de97f065957
Mark <markgonzalez02@hotmail.com>
2021-06-02 11:28:52 +0200

    Update SK-80 Finished popular workshop

--- app/src/main/java/com/example/homelayout/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/homelayout/ui/home/HomeFragment.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,11 +19,14 @@ import com.example.homelayout.ui.Cultureday.Form.culturedayFormFragment;
 import com.example.homelayout.ui.Cultureday.MainPage.CulturedayMainFragment;
 import com.example.homelayout.ui.contact.ContactFragment;
 import com.example.homelayout.ui.workshops.WorkshopsFragment;
+import com.example.homelayout.ui.workshops.WorkshopsPopular;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private Button btnBookWorkshop;
     private Button btnBookCultureDay;
+    private ConstraintLayout clPopularWorkshops;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -31,20 +35,27 @@ public class HomeFragment extends Fragment {
         btnBookWorkshop.setClickable(true);
         btnBookCultureDay = root.findViewById(R.id.button_book_culture_day);
         btnBookCultureDay.setClickable(true);
+        clPopularWorkshops = root.findViewById(R.id.home_item_popular_workshops);
 
-        btnBookWorkshop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new WorkshopsFragment()).commit();
-            }
-        });
+        btnBookWorkshop.setOnClickListener(this);
+        btnBookCultureDay.setOnClickListener(this);
+        clPopularWorkshops.setOnClickListener(this);
 
-        btnBookCultureDay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new culturedayFormFragment()).commit();
-            }
-        });
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_book_workshop:
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new WorkshopsFragment()).commit();
+                break;
+            case R.id.button_book_culture_day:
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new culturedayFormFragment()).commit();
+                break;
+            case R.id.home_item_popular_workshops:
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new WorkshopsPopular()).commit();
+                break;
+        }
     }
 }
