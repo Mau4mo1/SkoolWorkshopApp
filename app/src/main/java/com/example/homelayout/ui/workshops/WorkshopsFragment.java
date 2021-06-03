712a79786f57dc04099a8aea641570f4ca02b778
Noah de Keijzer <noah.cristiaan@gmail.com>
2021-06-03 13:52:43 +0200

    Update Strings and Workshops

--- app/src/main/java/com/example/homelayout/ui/workshops/WorkshopsFragment.java
+++ app/src/main/java/com/example/homelayout/ui/workshops/WorkshopsFragment.java
@@ -7,17 +7,77 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.homelayout.R;
+import com.example.homelayout.ui.Cultureday.MainPage.CulturedayMainFragment;
 
 public class WorkshopsFragment extends Fragment {
 
+    private ConstraintLayout mConstraintLayoutVisualArts;
+    private ConstraintLayout mConstraintLayoutDance;
+    private ConstraintLayout mConstraintLayoutMedia;
+    private ConstraintLayout mConstraintLayoutMusic;
+    private ConstraintLayout mConstraintLayoutSport;
+    private ConstraintLayout mConstraintLayoutTheatre;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_category_workshops, container, false);
+        View root = inflater.inflate(R.layout.fragment_category_workshops,container, false);
+        mConstraintLayoutVisualArts = root.findViewById(R.id.item_visual_arts);
+        mConstraintLayoutVisualArts.setClickable(true);
+        mConstraintLayoutVisualArts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopVisualArtsFragment()).commit();
+            }
+        });
+
+        mConstraintLayoutDance = root.findViewById(R.id.item_dance);
+        mConstraintLayoutDance.setClickable(true);
+        mConstraintLayoutDance.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopDanceFragment()).commit();
+            }
+        });
+
+        mConstraintLayoutMedia = root.findViewById(R.id.item_media);
+        mConstraintLayoutMedia.setClickable(true);
+        mConstraintLayoutMedia.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopMediaFragment()).commit();
+            }
+        });
+
+        mConstraintLayoutMusic = root.findViewById(R.id.item_music);
+        mConstraintLayoutMusic.setClickable(true);
+        mConstraintLayoutMusic.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopMusicFragment()).commit();
+            }
+        });
+
+        mConstraintLayoutSport = root.findViewById(R.id.item_sport);
+        mConstraintLayoutSport.setClickable(true);
+        mConstraintLayoutSport.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopSportFragment()).commit();
+            }
+        });
+
+        mConstraintLayoutTheatre = root.findViewById(R.id.item_theater);
+        mConstraintLayoutTheatre.setClickable(true);
+        mConstraintLayoutTheatre.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopTheatreFragment()).commit();
+            }
+        });
         return root;
     }
 }
