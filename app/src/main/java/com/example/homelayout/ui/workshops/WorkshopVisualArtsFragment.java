87de2474e1d4b520b520466136464156a6f647af
Wessel Kuijstermans <w.kuijstermans@wtudent.avans.nl>
2021-06-03 10:31:20 +0200

    Update SK-44 added clickables to visual arts workshops

--- app/src/main/java/com/example/homelayout/ui/workshops/WorkshopVisualArtsFragment.java
+++ app/src/main/java/com/example/homelayout/ui/workshops/WorkshopVisualArtsFragment.java
@@ -14,16 +14,49 @@ import androidx.fragment.app.Fragment;
 import com.example.homelayout.R;
 
 public class WorkshopVisualArtsFragment extends Fragment {
-private ImageButton mButtonGraffiti;
+    private ImageButton mButtonGraffiti;
+    private ImageButton mButtonGraffitiLight;
+    private ImageButton mButtonStopMotion;
+    private ImageButton mButtonTshirt;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_workshop_visual_arts, container, false);
-        mButtonGraffiti = root.findViewById(R.id.graffiti_Info);
+        mButtonGraffiti = root.findViewById(R.id.graffiti_info);
         mButtonGraffiti.setClickable(true);
+        mButtonGraffitiLight = root.findViewById(R.id.graffiti_light_info);
+        mButtonGraffitiLight.setClickable(true);
+        mButtonStopMotion = root.findViewById(R.id.stop_motion_info);
+        mButtonStopMotion.setClickable(true);
+        mButtonTshirt = root.findViewById(R.id.tshirt_design_info);
+        mButtonTshirt.setClickable(true);
         mButtonGraffiti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("https://skoolworkshop.nl/workshops/workshop-hiphop/");
+                Uri uri = Uri.parse("https://skoolworkshop.nl/workshops/workshop-graffiti/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        mButtonGraffitiLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://skoolworkshop.nl/workshops/workshop-light-graffiti/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        mButtonStopMotion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://skoolworkshop.nl/workshops/workshop-stop-motion/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        mButtonTshirt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://skoolworkshop.nl/workshops/workshop-t-shirt-ontwerpen/");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
