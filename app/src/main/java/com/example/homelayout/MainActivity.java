456839b6e4e2acd42514d1bdec12821df1e450cd
Noah de Keijzer <noah.cristiaan@gmail.com>
2021-06-03 11:34:09 +0200

    Revert "Merge remote-tracking branch 'origin/dev' into feature-logic-SK-88"
    This reverts commit 1d34284194bd3d33a3931f6d3d9e61f0f1dded1c.

--- app/src/main/java/com/example/homelayout/MainActivity.java
+++ app/src/main/java/com/example/homelayout/MainActivity.java
@@ -4,23 +4,22 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 import com.example.homelayout.ui.Cultureday.MainPage.CulturedayMainFragment;
-import com.example.homelayout.ui.Home.HomeFragment;
 import com.example.homelayout.ui.contact.ContactFragment;
 import com.example.homelayout.ui.workshops.WorkshopsForm;
 import com.example.homelayout.ui.workshops.WorkshopsFragment;
+import com.example.homelayout.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Passing each menu ID as a set of Ids because each
---
+++ app/src/main/java/com/example/homelayout/ui/workshops/WorkshopsVisualArts.java
@@ -0,0 +1,12 @@
+package com.example.homelayout.ui.workshops;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+
+public class WorkshopsVisualArts extends Fragment {
+    public WorkshopsVisualArts(){
+
+    }
+}
