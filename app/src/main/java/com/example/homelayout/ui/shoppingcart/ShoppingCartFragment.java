ff6f4c26de9307cc75cd125f0ac3f4c5c1554a02
Wessel Kuijstermans <w.kuijstermans@wtudent.avans.nl>
2021-06-08 11:58:19 +0200

    Update SK-92 worked on shopping cart xml

--- app/src/main/java/com/example/homelayout/ui/shoppingcart/ShoppingCartFragment.java
+++ app/src/main/java/com/example/homelayout/ui/shoppingcart/ShoppingCartFragment.java
@@ -1,6 +1,37 @@
 package com.example.homelayout.ui.shoppingcart;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.homelayout.R;
+import com.example.homelayout.ui.workshops.WorkshopsFragment;
+
+import org.jetbrains.annotations.NotNull;
+
 public class ShoppingCartFragment extends Fragment {
+
+    private Button mExtraWorkshopButton;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+
+        mExtraWorkshopButton = root.findViewById(R.id.btn_cart_extra_workshop);
+        mExtraWorkshopButton.setClickable(true);
+        mExtraWorkshopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new WorkshopsFragment()).commit();
+            }
+        });
+
+        return root;
+    }
 }
