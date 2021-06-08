1884d643c3dfb1ad8280ac7b8faabcb68a605675
Mart <martvanholten@hotmail.com>
2021-06-08 12:51:37 +0200

    Update SK-20

--- app/src/main/java/com/example/homelayout/ui/Cultureday/Form/CulturedayFormFragment.java
+++ app/src/main/java/com/example/homelayout/ui/Cultureday/Form/CulturedayFormFragment.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -37,9 +38,14 @@ public class CulturedayFormFragment extends Fragment {
     private EditText round_minutes;
     private Spinner dropdown_categories;
     public Context con;
-    private CheckBox workshop_check_dans;
+    private GridLayout workshop_check_dance;
+    private GridLayout workshop_check_sport;
+    private GridLayout workshop_check_media;
+    private GridLayout workshop_check_art;
+    private GridLayout workshop_check_music;
+    private GridLayout workshop_check_theater;
     private EditText workshop_per_round;
-    private ArrayList items;
+    private EditText workshop_rounds;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -52,12 +58,22 @@ public class CulturedayFormFragment extends Fragment {
         extra_price_participants_amount = root.findViewById(R.id.edn_cultureday_form_participants_shirt_and_drums_field);
         round_minutes = root.findViewById(R.id.edn_cultureday_form_time_per_round_field);
         workshop_per_round = root.findViewById(R.id.edn_cultureday_form_workshops_per_round_field);
+        workshop_rounds = root.findViewById(R.id.edn_cultureday_form_rounds_field);
 
         dropdown_categories = root.findViewById(R.id.sv_cultureday_form_catagorie_dropdown);
-        workshop_check_dans = root.findViewById(R.id.sv_cultureday_form_workshoplist_dans);
+        workshop_check_dance = root.findViewById(R.id.sv_cultureday_form_workshoplist_dance);
+        workshop_check_sport = root.findViewById(R.id.sv_cultureday_form_workshoplist_sport);
+        workshop_check_media = root.findViewById(R.id.sv_cultureday_form_workshoplist_media);
+        workshop_check_music = root.findViewById(R.id.sv_cultureday_form_workshoplist_music);
+        workshop_check_art = root.findViewById(R.id.sv_cultureday_form_workshoplist_visualarts);
+        workshop_check_theater = root.findViewById(R.id.sv_cultureday_form_workshoplist_theater);
         List<String> categories = new ArrayList<>();
         categories.add("Dans");
         categories.add("Sport");
+        categories.add("Beeldende kunst");
+        categories.add("Media");
+        categories.add("Muziek");
+        categories.add("Theater");
 
         ArrayAdapter<String> catagorie_dd = new ArrayAdapter<>(con, android.R.layout.simple_list_item_1, categories);
         catagorie_dd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -67,9 +83,47 @@ public class CulturedayFormFragment extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
                 if(dropdown_categories.getSelectedItemPosition() == 0) {
-                    workshop_check_dans.setVisibility(View.VISIBLE);
-                } else if(dropdown_categories.getSelectedItemPosition() == 1){
-                    workshop_check_dans.setVisibility(View.GONE);
+                    workshop_check_dance.setVisibility(View.VISIBLE);
+                    workshop_check_sport.setVisibility(View.GONE);
+                    workshop_check_media.setVisibility(View.GONE);
+                    workshop_check_music.setVisibility(View.GONE);
+                    workshop_check_art.setVisibility(View.GONE);
+                    workshop_check_theater.setVisibility(View.GONE);
+                }else if(dropdown_categories.getSelectedItemPosition() == 1){
+                    workshop_check_dance.setVisibility(View.GONE);
+                    workshop_check_sport.setVisibility(View.VISIBLE);
+                    workshop_check_media.setVisibility(View.GONE);
+                    workshop_check_music.setVisibility(View.GONE);
+                    workshop_check_art.setVisibility(View.GONE);
+                    workshop_check_theater.setVisibility(View.GONE);
+                }else if(dropdown_categories.getSelectedItemPosition() == 2){
+                    workshop_check_dance.setVisibility(View.GONE);
+                    workshop_check_sport.setVisibility(View.GONE);
+                    workshop_check_media.setVisibility(View.GONE);
+                    workshop_check_music.setVisibility(View.GONE);
+                    workshop_check_art.setVisibility(View.VISIBLE);
+                    workshop_check_theater.setVisibility(View.GONE);
+                }else if(dropdown_categories.getSelectedItemPosition() == 3){
+                    workshop_check_dance.setVisibility(View.GONE);
+                    workshop_check_sport.setVisibility(View.GONE);
+                    workshop_check_media.setVisibility(View.VISIBLE);
+                    workshop_check_music.setVisibility(View.GONE);
+                    workshop_check_art.setVisibility(View.GONE);
+                    workshop_check_theater.setVisibility(View.GONE);
+                }else if(dropdown_categories.getSelectedItemPosition() == 4){
+                    workshop_check_dance.setVisibility(View.GONE);
+                    workshop_check_sport.setVisibility(View.GONE);
+                    workshop_check_media.setVisibility(View.GONE);
+                    workshop_check_music.setVisibility(View.VISIBLE);
+                    workshop_check_art.setVisibility(View.GONE);
+                    workshop_check_theater.setVisibility(View.GONE);
+                }else if(dropdown_categories.getSelectedItemPosition() == 5){
+                    workshop_check_dance.setVisibility(View.GONE);
+                    workshop_check_sport.setVisibility(View.GONE);
+                    workshop_check_media.setVisibility(View.GONE);
+                    workshop_check_music.setVisibility(View.GONE);
+                    workshop_check_art.setVisibility(View.GONE);
+                    workshop_check_theater.setVisibility(View.VISIBLE);
                 }
             }
             @Override
@@ -98,8 +152,8 @@ public class CulturedayFormFragment extends Fragment {
         } catch (Exception e){
             AlertDialog.Builder minpopup_error = new AlertDialog.Builder(con);
             minpopup_error.setCancelable(true);
-            minpopup_error.setTitle("Aantal rondes");
-            minpopup_error.setMessage("Aantal rondes is geen nummer");
+            minpopup_error.setTitle("Aantal workshops per workshopronde");
+            minpopup_error.setMessage("Aantal workshops per workshopronde is geen nummer");
             minpopup_error.setNegativeButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
@@ -108,27 +162,27 @@ public class CulturedayFormFragment extends Fragment {
             });
             minpopup_error.show();
         }
+        try {
+            int work_rounds = Integer.valueOf(workshop_rounds.getText().toString());
+        } catch (Exception e){
+            AlertDialog.Builder roundpopup_error = new AlertDialog.Builder(con);
+            roundpopup_error.setCancelable(true);
+            roundpopup_error.setTitle("Aantal rondes");
+            roundpopup_error.setMessage("Aantal rondes is geen nummer");
+            roundpopup_error.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            roundpopup_error.show();
+        }
 
 
         btn_book_now_cdf.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    try {
-                        int minutes = Integer.valueOf(round_minutes.getText().toString());
-                    } catch (Exception e){
-                        AlertDialog.Builder minpopup_error = new AlertDialog.Builder(con);
-                        minpopup_error.setCancelable(true);
-                        minpopup_error.setTitle("Aantal rondes");
-                        minpopup_error.setMessage("Aantal rondes is geen nummer");
-                        minpopup_error.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        });
-                        minpopup_error.show();
-                    }
-                    if(Integer.valueOf(sub_total_cdf.getText().toString()) < 1255.50) {
+                    if(Integer.valueOf(sub_total_cdf.getText().toString()) != 1255.50) {
                         AlertDialog.Builder subpopup = new AlertDialog.Builder(con);
                         subpopup.setCancelable(true);
                         subpopup.setTitle("Subtotaal niet genoeg");
@@ -140,7 +194,7 @@ public class CulturedayFormFragment extends Fragment {
                             }
                         });
                         subpopup.show();
-                    }else if (Integer.valueOf(round_minutes.getText().toString()) < 60) {
+                    }else if (Integer.valueOf(round_minutes.getText().toString()) != 60) {
                         AlertDialog.Builder minpopup = new AlertDialog.Builder(con);
                         minpopup.setCancelable(true);
                         minpopup.setTitle("Workshops te kort");
