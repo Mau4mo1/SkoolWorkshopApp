29beb201c8724cb4059259d71a3f0e6dc512ea31
TimdeLaater <tdelaater@gmail.com>
2021-06-09 10:48:26 +0200

    Update SK-200 More menu

--- app/src/main/java/com/example/homelayout/MainActivity.java
+++ app/src/main/java/com/example/homelayout/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.homelayout;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.example.homelayout.ui.Cultureday.MainPage.CulturedayMainFragment;
 import com.example.homelayout.ui.home.HomeFragment;
@@ -54,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new ContactFragment();
                             break;
                         case R.id.navigation_more:
+
+
+                            //Initialize the Point with x, and y positions
+
+                            showStatusPopup(MainActivity.this);
                             selectedFragment = new HomeFragment();
                             break;
 
@@ -62,6 +75,29 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+    private void showStatusPopup(final Activity context) {
 
+        // Inflate the popup_layout.xml
+        LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.llSortChangePopup);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popupmenu, null);
+
+        // Creating the PopupWindow
+        PopupWindow changeStatusPopUp = new PopupWindow(context);
+        changeStatusPopUp.setContentView(layout);
+        changeStatusPopUp.setWidth(300);
+        changeStatusPopUp.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+        changeStatusPopUp.setFocusable(true);
+
+        // Some offset to align the popup a bit to the left, and a bit down, relative to button's position.
+        int OFFSET_X = 1000;
+        int OFFSET_Y = 1550;
+
+        //Clear the default translucent background
+        changeStatusPopUp.setBackgroundDrawable(new BitmapDrawable());
+
+        // Displaying the popup at the specified location, + offsets.
+        changeStatusPopUp.showAtLocation(layout, Gravity.NO_GRAVITY, OFFSET_X, OFFSET_Y);
+    }
 
 }
