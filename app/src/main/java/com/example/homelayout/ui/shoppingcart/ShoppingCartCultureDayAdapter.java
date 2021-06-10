e7664d259f85a322d45dbc36081fe8c6b9275e59
Wessel Kuijstermans <w.kuijstermans@wtudent.avans.nl>
2021-06-10 12:58:15 +0200

    Update SK-92 finalized recyclerview

--- app/src/main/java/com/example/homelayout/ui/shoppingcart/ShoppingCartCultureDayAdapter.java
+++ app/src/main/java/com/example/homelayout/ui/shoppingcart/ShoppingCartCultureDayAdapter.java
@@ -14,6 +14,7 @@ import com.example.homelayout.domain.CultureDayBooking;
 
 import java.io.Serializable;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCartCultureDayAdapter extends RecyclerView.Adapter<ShoppingCartCultureDayAdapter.CultureDayViewHolder> implements Serializable {
@@ -40,9 +41,20 @@ public class ShoppingCartCultureDayAdapter extends RecyclerView.Adapter<Shopping
         DecimalFormat decimalFormat = new DecimalFormat("#.00");
         CultureDayBooking cultureDayBooking = (CultureDayBooking) cultureDayData.get(pos);
         String prijs = decimalFormat.format(cultureDayBooking.getPrijs());
+        List<String> workshoplist = cultureDayBooking.getWorkshops();
+        String workshops = "";
+        for (int i = 0; i < workshoplist.size(); i++) {
+            if (i == workshoplist.size() - 1) {
+                workshops += workshoplist.get(i);
+            } else {
+                workshops += workshoplist.get(i) + "\n";
+            }
+        }
         holder.mRondes.setText(String.valueOf(cultureDayBooking.getRondes()));
         holder.mMinuten.setText(String.valueOf(cultureDayBooking.getMinutenPerRonde()));
         holder.mTotaleMinuten.setText(String.valueOf(cultureDayBooking.getTotaleMinuten()));
+        holder.mDeelnemers.setText(String.valueOf(cultureDayBooking.getDeelnemers()));
+        holder.mWorkshops.setText(workshops);
         holder.mTijdschema.setText(String.valueOf(cultureDayBooking.getTijdschema()));
         holder.mLeerniveau.setText(String.valueOf(cultureDayBooking.getLeerniveau()));
         holder.mDatum.setText(String.valueOf(cultureDayBooking.getDatum()));
@@ -53,6 +65,8 @@ public class ShoppingCartCultureDayAdapter extends RecyclerView.Adapter<Shopping
         private TextView mRondes;
         private TextView mMinuten;
         private TextView mTotaleMinuten;
+        private TextView mDeelnemers;
+        private TextView mWorkshops;
         private TextView mTijdschema;
         private TextView mLeerniveau;
         private TextView mDatum;
@@ -63,6 +77,8 @@ public class ShoppingCartCultureDayAdapter extends RecyclerView.Adapter<Shopping
             mRondes = (TextView) itemView.findViewById(R.id.shopping_cart_rondes);
             mMinuten = (TextView) itemView.findViewById(R.id.shopping_cart_minuten);
             mTotaleMinuten = (TextView) itemView.findViewById(R.id.shopping_cart_totale_minuten);
+            mDeelnemers = (TextView) itemView.findViewById(R.id.shopping_cart_deelnemers);
+            mWorkshops = (TextView) itemView.findViewById(R.id.shopping_cart_workshops);
             mTijdschema = (TextView) itemView.findViewById(R.id.shopping_cart_tijdschema);
             mLeerniveau = (TextView) itemView.findViewById(R.id.shopping_cart_leerniveau);
             mDatum = (TextView) itemView.findViewById(R.id.shopping_cart_datum);
--- app/src/main/java/com/example/homelayout/ui/shoppingcart/ShoppingCartFragment.java
+++ app/src/main/java/com/example/homelayout/ui/shoppingcart/ShoppingCartFragment.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.homelayout.R;
+import com.example.homelayout.domain.CultureDayBooking;
 import com.example.homelayout.domain.WorkshopBooking;
 import com.example.homelayout.ui.workshops.WorkshopsFragment;
 
@@ -25,24 +26,40 @@ public class ShoppingCartFragment extends Fragment {
     private Button mExtraWorkshopButton;
     private ImageButton mDeleteButton;
     private Context thisContext;
-    private LinearLayoutManager layoutManager;
-    private RecyclerView recyclerView;
+    private LinearLayoutManager workshopLayoutManager;
+    private LinearLayoutManager cultureDayLayoutManager;
+    private RecyclerView workshopRecyclerView;
+    private RecyclerView cultureDayRecyclerView;
     private ShoppingCartCultureDayAdapter shoppingCartCultureDayAdapter;
-    private ArrayList<WorkshopBooking> workshopBookingData = new ArrayList<>();
+    private ShoppingCartWorkshopAdapter shoppingCartWorkshopAdapter;
+    private ArrayList<WorkshopBooking> workshopBookings = new ArrayList<>();
+    private ArrayList<CultureDayBooking> cultureDayBookings = new ArrayList<>();
+    private ArrayList<String> workshops = new ArrayList<>();
     private WorkshopBooking workshopDummyData = new WorkshopBooking("workshop rap", 2, 75, "voorbeeld", "voorbeeld", "3 Juni 2021", 300.00);
+    private CultureDayBooking cultureDayDummyData = new CultureDayBooking(3,3,60,75, workshops, "voorbeeld", "HBO", "26 Juni 2022",1500.00);
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+        workshops.add("workshop rap");
+        workshops.add("workshop graffiti");
+        workshops.add("workshop stepping");
         thisContext = container.getContext();
-        layoutManager = new LinearLayoutManager(thisContext);
-        recyclerView = root.findViewById(R.id.shopping_cart_recycler);
-        recyclerView.setLayoutManager(layoutManager);
-        workshopBookingData.add(workshopDummyData);
-        shoppingCartCultureDayAdapter = new ShoppingCartCultureDayAdapter(workshopBookingData);
-        recyclerView.setAdapter(shoppingCartCultureDayAdapter);
+        workshopLayoutManager = new LinearLayoutManager(thisContext);
+        workshopRecyclerView = root.findViewById(R.id.shopping_cart_workshop_recycler);
+        workshopRecyclerView.setLayoutManager(workshopLayoutManager);
+        workshopBookings.add(workshopDummyData);
+        shoppingCartWorkshopAdapter = new ShoppingCartWorkshopAdapter(workshopBookings);
+        workshopRecyclerView.setAdapter(shoppingCartWorkshopAdapter);
+        cultureDayLayoutManager = new LinearLayoutManager(thisContext);
+        cultureDayRecyclerView = root.findViewById(R.id.shopping_cart_culture_day_recycler);
+        cultureDayRecyclerView.setLayoutManager(cultureDayLayoutManager);
+        cultureDayBookings.add(cultureDayDummyData);
+        shoppingCartCultureDayAdapter = new ShoppingCartCultureDayAdapter(cultureDayBookings);
+        cultureDayRecyclerView.setAdapter(shoppingCartCultureDayAdapter);
+
 
         mExtraWorkshopButton = root.findViewById(R.id.btn_cart_extra_workshop);
         mExtraWorkshopButton.setClickable(true);
