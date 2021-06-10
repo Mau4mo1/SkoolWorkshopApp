c1e6c33895a58fd1ca8cd4dd36fc48b3b7a2f55f
luuk <luuk-bartels@hotmail.com>
2021-06-10 16:26:24 +0200

    Added logic to firebaselogic

--- app/src/main/java/com/example/homelayout/logic/FireBaseLogic.java
+++ app/src/main/java/com/example/homelayout/logic/FireBaseLogic.java
@@ -1,12 +1,25 @@
 package com.example.homelayout.logic;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 
 public class FireBaseLogic extends FirebaseMessagingService {
 
-    FirebaseMessaging
+    @Override
+    public void onNewToken(@NonNull String s) {
+        Log.d("TAG", "The token is refreshed" + s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    
 }
