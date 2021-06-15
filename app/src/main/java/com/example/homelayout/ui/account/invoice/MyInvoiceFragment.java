393d38729544d28f22193cafe1d553eb584a1582
Mart <martvanholten@hotmail.com>
2021-06-15 15:09:51 +0200

    Update SK-233 added logic

---
+++ app/src/main/java/com/example/homelayout/ui/account/invoice/MyInvoiceFragment.java
@@ -0,0 +1,45 @@
+package com.example.homelayout.ui.account.invoice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.homelayout.R;
+import com.example.homelayout.ui.home.HomeFragment;
+
+public class MyInvoiceFragment extends Fragment {
+    private TextView tv_invoice_edit;
+    private TextView tv_workshop_edit;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_cultureday_form, container, false);
+        tv_invoice_edit = root.findViewById(R.id.tv_my_invoice_edit_invoice_data);
+        tv_workshop_edit = root.findViewById(R.id.tv_my_invoice_edit_workshopinfo_data);
+
+        tv_invoice_edit.setClickable(true);
+        tv_invoice_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                PLACEHOLDER NEEDS TO CHANGE DATA
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new InvoiceDataEditFragment());
+            }
+        });
+
+        tv_workshop_edit.setClickable(true);
+        tv_workshop_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                PLACEHOLDER NEEDS TO CHANGE DATA
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new BookedWorkshopDataEditFragment());
+            }
+        });
+
+        return root;
+    }
+}
