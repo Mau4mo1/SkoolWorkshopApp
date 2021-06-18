97c88df42f262d2650956319b5fdaebf8b48feb8
luuk <luuk-bartels@hotmail.com>
2021-06-18 12:14:34 +0200

    Added validation to the register

--- app/src/main/java/com/example/homelayout/ui/register/RegisterFragment.java
+++ app/src/main/java/com/example/homelayout/ui/register/RegisterFragment.java
@@ -12,19 +12,40 @@ import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.homelayout.MainActivity;
 import com.example.homelayout.R;
+import com.example.homelayout.logic.Validation;
+import com.example.homelayout.ui.home.HomeFragment;
 
 public class RegisterFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_register, container, false);
         Context thiscontext = container.getContext();
         TextView txtView = (TextView)root.findViewById(R.id.tv_register_policy);
+        EditText email = (EditText)root.findViewById(R.id.email_register);
+        EditText password = (EditText)root.findViewById(R.id.password_register);
+        Button sendButton = (Button)root.findViewById(R.id.btn_send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(new Validation().validateEmail(email.getText().toString()) && new Validation().validatePassword(password.getText().toString())){
+                    ((MainActivity)getActivity()).setLoggedIn(true);
+                    Toast.makeText(thiscontext, "Username: " + email.getText().toString() + " & Password: "+ password.getText().toString(), Toast.LENGTH_SHORT).show();
+                    getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new HomeFragment()).addToBackStack(null).commit();
+                }else{
+                    Toast.makeText(thiscontext,  "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
         String text = "Je persoonlijke gegevens worden gebruikt om je ervaringen op deze site te ondersteunen, om toegang tot je account te beheren en voor andere doeleinden zoals omschreven in ons privacybeleid";
         SpannableString ss= new SpannableString(text);
         ClickableSpan clickableSpan1= new ClickableSpan() {
